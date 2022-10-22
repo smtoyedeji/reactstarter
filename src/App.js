c8d5f922a759823656cab13213d3bcbfc0378919
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-10-22 10:33:01 +0100

    useEffect-1 commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics'
+
+
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
+      <UseEffectBasics />
     </div>
   )
 }
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,22 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  console.log("render component");
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You have clicked ${count} times`;
+    console.log("useEffect ran")
+  });
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button className='btn' onClick={() => setCount(count + 1)}>
+        click me
+      </button> 
+    </>
+  );
 };
 
 export default UseEffectBasics;
+
