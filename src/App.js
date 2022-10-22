180c9cea1037b2e6010754b447e60e9f64082b01
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-10-22 11:31:38 +0100

    useEffect3 commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, {useState} from 'react'
 // import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics'
-import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+// import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+import UseEffectFetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 
 
 function App() {
-  const [show, setShow] = useState(true)
+  // const [show, setShow] = useState(true)
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
       {/* <UseEffectBasics /> */}
-      {show && <UseEffectCleanup />}
-      <button className='btn' onClick={() => setShow(!show)}>toggle show</button>
+      {/* {show && <UseEffectCleanup />} */}
+      {/* <button className='btn' onClick={() => setShow(!show)}>toggle show</button> */}
+      <UseEffectFetchData />
     </div>
   )
 }
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,36 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  const displayUsers = users.map(user => {
+    const { id, login, avatar_url, html_url } = user;
+    return (
+      <li key={id}>
+        <img src={avatar_url} alt={login} />
+        <div>
+          <h4>{login}</h4>
+          <a href={html_url}>profile</a>  
+        </div>
+      </li>
+    );
+  })
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(users => setUsers(users));
+  }, [])
+
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className="users">
+        {displayUsers}
+      </ul>
+    </>
+  )
 };
 
 export default UseEffectFetchData;
